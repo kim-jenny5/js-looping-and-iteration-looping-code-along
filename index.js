93ddfcf0bc69850f34e954d097c5252ce27da6ff
Jenny Kim <kimj1905@gmail.com>
2021-08-18 16:03:28 -0400

    commit tree 24faa7c541a08b1b6649a86310de86e1f4861611

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let thanking = []
+    for (let i = 0; i < array.length; i++) {
+        thanking.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return thanking
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int--)
+    }
+}
